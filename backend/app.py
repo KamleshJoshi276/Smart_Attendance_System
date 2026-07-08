8bb3010d9b744c0299ecb6bd94f2c7221e27eb9a
Kamlesh Joshi <kamal9690joshi@gmail.com>
2026-07-08 17:29:35 +0530

    Add default admin accounts

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,12 @@ import sys
 from flask import Flask, jsonify, send_from_directory
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from backend.config import Config
+from backend.models import Teacher
 from backend.extensions import init_extensions, db, socketio
 from backend.routes.auth import auth_bp
 from backend.routes.attendance import attendance_bp
 from backend.routes.admin import admin_bp
+
 from backend.services.face_verification import ensure_face_directories, train_recognizer
 
 def create_app():
@@ -19,39 +21,26 @@ def create_app():
     init_extensions(app)
 
     with app.app_context():
-        ensure_face_directories()
-        # db.create_all()   # Commented for TiDB
-        train_recognizer()
+     ensure_face_directories()
 
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(attendance_bp)
-    app.register_blueprint(admin_bp)
+    # Default Admin 1
+    if not Teacher.query.filter_by(teacher_id="Kamal@123").first():
+        admin1 = Teacher(
+            teacher_id="Kamal@123",
+            name="Kamal Joshi"
+        )
+        admin1.set_password("Kamal@123")
+        db.session.add(admin1)
 
-    @app.route('/uploads/profile/<filename>')
-    def profile_image(filename):
-        return send_from_directory(app.config['PROFILE_UPLOAD_FOLDER'], filename)
+    # Default Admin 2
+    if not Teacher.query.filter_by(teacher_id="Ajay@123").first():
+        admin2 = Teacher(
+            teacher_id="Ajay@123",
+            name="Ajay Rawat"
+        )
+        admin2.set_password("Ajay@123")
+        db.session.add(admin2)
 
-    @app.route('/uploads/attendance/<filename>')
-    def attendance_image(filename):
-        return send_from_directory(app.config['ATTENDANCE_UPLOAD_FOLDER'], filename)
+    db.session.commit()
 
-    @app.route('/', defaults={'path': ''})
-    @app.route('/<path:path>')
-    def catch_all(path):
-        if os.path.exists(os.path.join(app.static_folder, path)):
-            return send_from_directory(app.static_folder, path)
-        return send_from_directory(app.static_folder, 'index.html')
-    @app.errorhandler(404)
-    def handle_404(e):
-        return jsonify({'message': 'Resource not found'}), 404
-
-    return app
-app = create_app()
-
-if __name__ == '__main__':
-    socketio.run(
-        app,
-        host='0.0.0.0',
-        port=int(os.environ.get('PORT', 5000)),
-        debug=True
-    )
+    train_recognizer()
